b142c4327f9668e256a4fe310f4445e15fc92fd9
AnDang <81478990+an7bht@users.noreply.github.com>
2022-03-31 16:52:15 +0700

    hhh

--- database.js
+++ database.js
@@ -12,12 +12,9 @@
 var mysql = require('mysql');
 var pool  = mysql.createPool({
   connectionLimit : 10,
-  host            : 'dkhpiuh.herokuapp.com',
+  host            : 'localhost',
   user            : 'root',
   password        : '123456',
   database        : 'sqlquanlyhocphan'
 });
-pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-  });
+module.exports=pool;
