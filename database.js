467036029c1d7e828d178787c03a25142721a8bd
AnDang <81478990+an7bht@users.noreply.github.com>
2022-03-31 17:03:59 +0700

    hi

--- database.js
+++ database.js
@@ -1,20 +1,20 @@
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-
-//     host:'localhost',
-//     user:'root',
-//     password:'123456',
-//     database:'sqlquanlyhocphan'  
-// });
-
-// module.exports=connection;
-
 var mysql = require('mysql');
-var pool  = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'localhost',
-  user            : 'root',
-  password        : '123456',
-  database        : 'sqlquanlyhocphan'
+var connection = mysql.createConnection({
+
+    host:'localhost',
+    user:'root',
+    password:'123456',
+    database:'sqlquanlyhocphan'  
 });
-module.exports=pool;
+
+module.exports=connection;
+
+// var mysql = require('mysql');
+// var pool  = mysql.createPool({
+//   connectionLimit : 10,
+//   host            : 'localhost',
+//   user            : 'root',
+//   password        : '123456',
+//   database        : 'sqlquanlyhocphan'
+// });
+// module.exports=pool;
