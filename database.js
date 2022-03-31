032b481ab2fb8903dfe13f1dc1381dd56807fd22
AnDang <81478990+an7bht@users.noreply.github.com>
2022-03-31 16:57:22 +0700

    h

--- database.js
+++ database.js
@@ -12,7 +12,7 @@
 var mysql = require('mysql');
 var pool  = mysql.createPool({
   connectionLimit : 10,
-  host            : 'dkhpiuh.herokuapp.com',
+  host            : 'localhost',
   user            : 'root',
   password        : '123456',
   database        : 'sqlquanlyhocphan'
