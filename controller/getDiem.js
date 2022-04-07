3404dbc55ae859ea0a2ce50e0c0f8ee00b9f8412
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-07 22:30:10 +0700

    hi

--- controller/getDiem.js
+++ controller/getDiem.js
@@ -4,7 +4,7 @@ var Diem={
 	getAllDiem:function(callback){
 		return database.query("Select * from phieudangkylhp",callback);
 	},getDiemById:function(id,callback){
-		return database.query("select lophocphan.Nam, lophocphan.hocky, monhocphan.TenMHHP,monhocphan.SoTinChi, phieudangkylhp.DiemTK, phieudangkylhp.DiemGK, phieudangkylhp.DiemTH ,phieudangkylhp.DiemCK from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP where phieudangkylhp.MSSV = ? and phieudangkylhp.Nhom ='LT' order by lophocphan.Nam asc , lophocphan.HocKy asc;",[id],callback);
+		return database.query("select lophocphan.Nam, lophocphan.HocKy, monhocphan.TenMHHP,monhocphan.SoTinChi, phieudangkylhp.DiemTK, phieudangkylhp.DiemGK, phieudangkylhp.DiemTH ,phieudangkylhp.DiemCK from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP where phieudangkylhp.MSSV = ? and phieudangkylhp.Nhom ='LT' order by lophocphan.Nam asc , lophocphan.HocKy asc;",[id],callback);
 	}
 };
  module.exports=Diem;
