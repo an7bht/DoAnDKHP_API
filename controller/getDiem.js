2c0a78133f72b2cedf7426e5b207399ddcb4a50c
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-08 21:13:11 +0700

    hihi

--- controller/getDiem.js
+++ controller/getDiem.js
@@ -3,8 +3,8 @@ let database = require('../database');
 var Diem={
 	getAllDiem:function(callback){
 		return database.query("Select * from phieudangkylhp",callback);
-	},getDiemById:function(id,callback){
-		return database.query("select lophocphan.Nam, lophocphan.HocKy, monhocphan.TenMHHP,monhocphan.SoTinChi, phieudangkylhp.DiemTK, phieudangkylhp.DiemGK, phieudangkylhp.DiemTH ,phieudangkylhp.DiemCK from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP where phieudangkylhp.MSSV = ? and phieudangkylhp.Nhom ='LT' order by lophocphan.Nam asc , lophocphan.HocKy asc;",[id],callback);
+	},getDiemById:function(MSSV,callback){
+		return database.query("select lophocphan.Nam, lophocphan.HocKy, monhocphan.TenMHHP,monhocphan.SoTinChi, phieudangkylhp.DiemTK, phieudangkylhp.DiemGK, phieudangkylhp.DiemTH ,phieudangkylhp.DiemCK from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP where phieudangkylhp.Nhom ='LT' and phieudangkylhp.MSSV = ? order by lophocphan.Nam asc , lophocphan.HocKy asc;",[MSSV],callback);
 	}
 };
  module.exports=Diem;
--- controller/getLichHoc.js
+++ controller/getLichHoc.js
@@ -2,7 +2,7 @@ let database = require('../database');
 
 var LichHoc={
 	getLichHoc:function(HocKy, Nam, MSSV,callback){
-		return database.query("select lophocphan.MaLopHP, thoigian_phonghoc_giangvien.TietHoc, thoigian_phonghoc_giangvien.NgayHoc, thoigian_phonghoc_giangvien.PhongHoc, phieudangkylhp.Nhom, monhocphan.TenMHHP, giangvien.HoTen from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join thoigian_phonghoc_giangvien on thoigian_phonghoc_giangvien.MaLopHP = lophocphan.MaLopHP and thoigian_phonghoc_giangvien.MaNhom= phieudangkylhp.Nhom inner join monhocphan on  monhocphan.MaMHP = lophocphan.MaMHP inner join giangvien on giangvien.MaGV = thoigian_phonghoc_giangvien.MaGV where phieudangkylhp.MSSV = ? and lophocphan.HocKy = ? and lophocphan.Nam=? order by thoigian_phonghoc_giangvien.TietHoc;",[HocKy], [Nam], [MSSV],callback);
+		return database.query("select lophocphan.MaLopHP, thoigian_phonghoc_giangvien.TietHoc, thoigian_phonghoc_giangvien.NgayHoc, thoigian_phonghoc_giangvien.PhongHoc, phieudangkylhp.Nhom, monhocphan.TenMHHP, giangvien.HoTen from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join thoigian_phonghoc_giangvien on thoigian_phonghoc_giangvien.MaLopHP = lophocphan.MaLopHP and thoigian_phonghoc_giangvien.MaNhom= phieudangkylhp.Nhom inner join monhocphan on  monhocphan.MaMHP = lophocphan.MaMHP inner join giangvien on giangvien.MaGV = thoigian_phonghoc_giangvien.MaGV where lophocphan.HocKy = ? and lophocphan.Nam = ? phieudangkylhp.MSSV = ? order by thoigian_phonghoc_giangvien.TietHoc;",[HocKy], [Nam], [MSSV],callback);
 	}
 }
  module.exports=LichHoc;
