892cd13167f0289a39875ca54e61c1d26c1fd1a7
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-09 08:24:42 +0700

    update Diem

--- controller/getDiem.js
+++ controller/getDiem.js
@@ -4,7 +4,7 @@ var Diem={
 	getAllDiem:function(callback){
 		return database.query("Select * from phieudangkylhp",callback);
 	},getDiemById:function(MSSV,callback){
-		return database.query("select lophocphan.Nam, lophocphan.HocKy, monhocphan.TenMHHP,monhocphan.SoTinChi, phieudangkylhp.DiemTK, phieudangkylhp.DiemGK, phieudangkylhp.DiemTH ,phieudangkylhp.DiemCK from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP where phieudangkylhp.MSSV = ? order by lophocphan.Nam asc;",[MSSV],callback);
+		return database.query("SELECT lophocphan.Nam, lophocphan.HocKy, monhocphan.TenMHHP, monhocphan.SoTinChi, phieudangkylhp.DiemTK, phieudangkylhp.DiemGK, phieudangkylhp.DiemTH ,phieudangkylhp.DiemCK FROM phieudangkylhp inner join lophocphan  on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP where MSSV = ? order by Nam",[MSSV],callback);
 	}
 };
  module.exports=Diem;
