cbb4c69a42e011a246c6ae69d28af75c1d0c92cc
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-11 19:55:40 +0700

    uploaddkhp

--- controller/postDKHP.js
+++ controller/postDKHP.js
@@ -3,7 +3,7 @@ let database = require('../database');
 var DKHP={
 	postDKHP:function(MSSV,MaLopHP,Nhom,callback){
 			console.log(MSSV, MaLopHP, Nhom)
-		return database.query("INSERT INTO `sqlquanlyhocphan`.`phieudangkylhp` (`MSSV`, `MaLopHP`, `Nhom`) VALUES (?, ?, ?)",[MSSV,MaLopHP,Nhom],callback);
+		return database.query("INSERT INTO `phieudangkylhp` (`MSSV`, `MaLopHP`, `Nhom`) VALUES (?, ?, ?)",[MSSV,MaLopHP,Nhom],callback);
 	}
 }
  module.exports=DKHP;
--- controller/postTaiKhoanSV.js
+++ controller/postTaiKhoanSV.js
@@ -3,7 +3,7 @@ let database = require('../database');
 var TaiKhoanSV1={
 	postTaKhoanSV:function(MSSV,DiaChi,callback){
 			console.log(MSSV)
-		return database.query("INSERT INTO `sqlquanlyhocphan`.`sinhvien` (`MSSV`, `DiaChi`) VALUES (?,?) ",[MSSV,DiaChi],callback);
+		return database.query("INSERT INTO `sinhvien` (`MSSV`, `DiaChi`) VALUES (?,?) ",[MSSV,DiaChi],callback);
 	}
 }
  module.exports=TaiKhoanSV1;
