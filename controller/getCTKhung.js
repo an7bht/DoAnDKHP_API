57e7112fa554c3e343cf02045c72e94737e3a048
AnDang <81478990+an7bht@users.noreply.github.com>
2022-05-03 00:37:50 +0700

    make it better

--- controller/getCTKhung.js
+++ controller/getCTKhung.js
@@ -2,7 +2,7 @@ let database = require('../database');
 
 var CTK={
 	getCTKhung:function(MSSV, HocKy,callback){
-		return database.query("select chuyennganh.TenChuyenNganh,monhocphan.MaMHP,monhocphan.TenMHHP , chuongtrinhkhung.HocKy from sinhvien_thuoc_nganh inner join chuongtrinhkhung on sinhvien_thuoc_nganh.MaChuyenNganh  = chuongtrinhkhung.MachuyenNganh inner join monhocphan on monhocphan.MaMHP  = chuongtrinhkhung.MaMHP inner join chuyennganh on chuyennganh.MaChuyenNganh = sinhvien_thuoc_nganh.MaChuyenNganh where sinhvien_thuoc_nganh.MSSV = ? and chuongtrinhkhung.HocKy = ? order by  monhocphan.TenMHHP",[MSSV, HocKy],callback);
+		return database.query("select chuyennganh.TenChuyenNganh,monhocphan.MaMHP,monhocphan.TenMHHP , monhocphan.SoTinChi from sinhvien_thuoc_nganh inner join chuongtrinhkhung on sinhvien_thuoc_nganh.MaChuyenNganh  = chuongtrinhkhung.MachuyenNganh inner join monhocphan on monhocphan.MaMHP  = chuongtrinhkhung.MaMHP inner join chuyennganh on chuyennganh.MaChuyenNganh = sinhvien_thuoc_nganh.MaChuyenNganh where sinhvien_thuoc_nganh.MSSV = ? and chuongtrinhkhung.HocKy = ? order by  monhocphan.TenMHHP",[MSSV, HocKy],callback);
 	}
 }
  module.exports=CTK;
