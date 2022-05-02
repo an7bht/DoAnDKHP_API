22f40caa514af6ff4de8de497582969d1005372c
AnDang <81478990+an7bht@users.noreply.github.com>
2022-05-02 21:27:26 +0700

    make it better

--- controller/getDiem.js
+++ controller/getDiem.js
@@ -3,8 +3,8 @@ let database = require('../database');
 var Diem={
 	getAllDiem:function(callback){
 		return database.query("Select * from phieudangkylhp",callback);
-	},getDiemById:function(MSSV,callback){
-		return database.query("select lophocphan.Nam, lophocphan.hocky, monhocphan.TenMHHP,monhocphan.SoTinChi, phieudangkylhp.DiemTK, phieudangkylhp.DiemGK, phieudangkylhp.DiemTH ,phieudangkylhp.DiemCK from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP  where  phieudangkylhp.MSSV = ? and phieudangkylhp.Nhom = 'LT' order by lophocphan.Nam asc",[MSSV],callback);
+	},getDiemById:function(MSSV, Nam, hocky,callback){
+		return database.query("select lophocphan.Nam, lophocphan.hocky, monhocphan.TenMHHP,monhocphan.SoTinChi, phieudangkylhp.DiemTK, phieudangkylhp.DiemGK, phieudangkylhp.DiemTH ,phieudangkylhp.DiemCK from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP  where  phieudangkylhp.MSSV = ? and lophocphan.Nam = ? and lophocphan.hocky=? and phieudangkylhp.Nhom='LT' order by lophocphan.Nam",[MSSV, Nam, hocky],callback);
 	}
 };
  module.exports=Diem;
