0f8b7b370c11f34dbc3e604f0850c6364ecc2e73
AnDang <81478990+an7bht@users.noreply.github.com>
2022-05-02 21:52:52 +0700

    make it better

--- controller/getCongNo.js
+++ controller/getCongNo.js
@@ -2,7 +2,7 @@ let database = require('../database');
 
 var CongNo={
 	getCongNo:function(MSSV, Nam, HocKy,callback){
-		return database.query("select monhocphan.TenMHHP, monhocphan.SoTinChi, lophocphan.Nam, lophocphan.HocKy from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP  where  phieudangkylhp.MSSV = ? and lophocphan.Nam = ? and lophocphan.HocKy = ? and phieudangkylhp.Nhom='LT' order by monhocphan.TenMHHP",[MSSV, Nam, HocKy],callback);
+		return database.query("select monhocphan.TenMHHP, monhocphan.SoTinChi from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP  where  phieudangkylhp.MSSV = ? and lophocphan.Nam = ? and lophocphan.HocKy = ? and phieudangkylhp.Nhom='LT' order by monhocphan.TenMHHP",[MSSV, Nam, HocKy],callback);
 	}
 }
  module.exports=CongNo;
--- controller/getDiem.js
+++ controller/getDiem.js
@@ -4,7 +4,7 @@ var Diem={
 	getAllDiem:function(callback){
 		return database.query("Select * from phieudangkylhp",callback);
 	},getDiemById:function(MSSV, Nam, hocky,callback){
-		return database.query("select lophocphan.Nam, lophocphan.hocky, monhocphan.TenMHHP,monhocphan.SoTinChi, phieudangkylhp.DiemTK, phieudangkylhp.DiemGK, phieudangkylhp.DiemTH ,phieudangkylhp.DiemCK from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP  where  phieudangkylhp.MSSV = ? and lophocphan.Nam = ? and lophocphan.hocky=? and phieudangkylhp.Nhom='LT' order by monhocphan.TenMHHP",[MSSV, Nam, hocky],callback);
+		return database.query("select monhocphan.TenMHHP,monhocphan.SoTinChi, phieudangkylhp.DiemTK, phieudangkylhp.DiemGK, phieudangkylhp.DiemTH ,phieudangkylhp.DiemCK from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP  where  phieudangkylhp.MSSV = ? and lophocphan.Nam = ? and lophocphan.hocky=? and phieudangkylhp.Nhom='LT' order by monhocphan.TenMHHP",[MSSV, Nam, hocky],callback);
 	}
 };
  module.exports=Diem;
