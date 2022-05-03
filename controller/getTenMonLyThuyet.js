f843e07de8b758a6a77c17258622fd4fc53c4a81
AnDang <81478990+an7bht@users.noreply.github.com>
2022-05-03 12:43:11 +0700

    gsd

---
+++ controller/getTenMonLyThuyet.js
@@ -0,0 +1,8 @@
+let database = require('../database');
+
+var TenMonLyThuyet={
+	getTenMonLyThuyet:function(MSSV,Nam, HocKy,callback){
+		return database.query("select monhocphan.TenMHHP from phieudangkylhp inner join lophocphan on lophocphan.MaLopHP = phieudangkylhp.MaLopHP inner join monhocphan on monhocphan.MaMHP = lophocphan.MaMHP  where  phieudangkylhp.MSSV =? and lophocphan.Nam =? and lophocphan.hocky=? and phieudangkylhp.Nhom ='LT'",[MSSV,Nam, HocKy],callback);
+	}
+}
+ module.exports=TenMonLyThuyet;
